45a30df73f5a479cdce8cb580de0568e50a4d11d
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-07-03 16:34:19 +0300

    add router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <Hero title="Find your dream job" subtitle="Never give up on your dreams" />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
